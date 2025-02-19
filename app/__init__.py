cee2b8ceb70932ee61f6b73cd9411157dc363eb7
Gabriel M <gabrielalmeidamaciel194@gmail.com>
2025-02-19 14:50:22 -0300

    conexão com o db consertada

--- app/__init__.py
+++ app/__init__.py
@@ -1,23 +1,20 @@
-# arquivo para inicialização de 'app' como módulo
 import dotenv, os
 from flask import Flask
 from app.extensions import mongo
 from flask_login import LoginManager
 
-from app.blueprints.auth import auth_bp
-from app.blueprints.menu import menu_bp
-from app.blueprints.recipe import recipe_bp
-
-from app.models.users import User
 
 def create_app() -> Flask:
     app = Flask(__name__)
 
     dotenv.load_dotenv(dotenv.find_dotenv()) # carrega as variaveis de ambiente
     app.secret_key = os.getenv("SECRET_KEY")
-
-    app.config["MONGO_URI"] = "mongodb://localhost:27017/culinaria"
+    app.config["MONGO_URI"] = os.getenv("MONGO_URI")
     mongo.init_app(app)
+
+    from app.blueprints.auth import auth_bp
+    from app.blueprints.menu import menu_bp
+    from app.blueprints.recipe import recipe_bp
 
     app.register_blueprint(auth_bp)
     app.register_blueprint(menu_bp)
